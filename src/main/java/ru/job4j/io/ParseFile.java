d63fa6168c591ddb8bce4779c5ae1acdafd2f646
kondratkovAnton <kondratkov.anton@gmail.com>
2023-10-24 18:16:17 +0700

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.function.Predicate;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public final class ParseFile {
+    @GuardedBy("this")
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized String getContent(Predicate<Character> filter) {
+        String output = "";
+        try (InputStream i = new FileInputStream(file)) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return output;
+    }
+}
---
+++ src/main/java/ru/job4j/io/saveContentToFile.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public final class saveContentToFile {
+    @GuardedBy("this")
+    private final File file;
+
+    public saveContentToFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) {
+        try (OutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
