a80ba0958523e033b96140d9cc0ac032f8b6626e
Anton Kondratkov <kondratkov.anton@mail.ru>
2024-06-08 22:08:26 +0700

    Добавление файла OneGit

---
+++ src/main/java/ru/job4j/git/OneGit.java
@@ -0,0 +1,10 @@
+package ru.job4j.git;
+
+/**
+ * Откат изменений в Git.
+ */
+public class OneGit {
+    public static void main(String[] args) {
+        System.out.println("Hello from OneGit");
+    }
+}
