7862f8417debbf68d8d130d62f57c3573f8ab6d4
Anton Kondratkov <kondratkov.anton@mail.ru>
2024-07-10 20:07:10 +0700

    Test git stash after merge

--- src/main/java/ru/job4j/git/TestStash.java
+++ src/main/java/ru/job4j/git/TestStash.java
@@ -3,11 +3,5 @@ package ru.job4j.git;
 public class TestStash {
     public static void main(String[] args) {
         System.out.println("Testing git stash before...");
-
-    }
-
-    @Override
-    public String toString() {
-        return "TestStash{}";
     }
 }
